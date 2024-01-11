8759aeb3f74997d1bdbdad6587a581f0dec05373
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-11 10:12:32 +0000

    complete json file

---
+++ server.js
@@ -0,0 +1,8 @@
+[
+  {
+    riddle:
+      "What has roots as nobody sees, Is taller than trees, Up, up it goes, And yet never grows?",
+    "correct-answer": "Mountain",
+    "incorrect-answer": [{ answer: "example" }, { answer: "example 2" }],
+  },
+];
